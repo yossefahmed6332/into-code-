bdda2c932b1773bd5ce08feec050cbcdd3d53de8
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-05-07 13:30:15 +0300

    create first line in java code

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-//set server 
-const express= require('express');
-const app= express();
-app.set('view engine','ejs');
+const express = require('express');
+const app = express();
 
+app.set('view engine', 'ejs');
 
-//set routes 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/class', (req, res) => {
+    const className = req.query.className || "MyClass";
+    const classType = req.query.classType || "public class";
+    const isExtended = req.query.isExtended === "true";
+    const ParentClass = req.query.ParentClass || "";
 
-//set server port
-app.listen(3000,()=>{
+    let code = "";
+
+    if (isExtended && ParentClass) {
+        code = `${classType} ${className} extends ${ParentClass}\n {`;
+    } else {
+        code = `${classType} ${className} {\n`;
+    }
+
+
+    res.render('class', {code});
+});
+
+app.listen(3000, () => {
     console.log('server is running on port 3000');
 });
