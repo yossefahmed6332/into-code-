4ed1aa0bbfcf023c2eee871775ff84b7039c8a13
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-05-09 08:51:31 +0300

    solve problems

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -7,25 +8,82 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/class', (req, res) => {
+
+
+function generateCode(req) {
+
     const className = req.query.className || "MyClass";
+
     const classType = req.query.classType || "public class";
+
     const isExtended = req.query.isExtended === "true";
+
     const ParentClass = req.query.ParentClass || "";
 
     let code = "";
 
+
+
     if (isExtended && ParentClass) {
-        code = `${classType} ${className} extends ${ParentClass}\n {`;
+
+        code = `${classType} ${className} extends ${ParentClass} {\n`;
+
     } else {
+
         code = `${classType} ${className} {\n`;
+
     }
 
 
-    res.render('class', {code});
+
+    code += getAttributes(req);
+
+
+
+    code += `\n}`;
+
+    return code;
+}
+
+
+
+function getAttributes(req) {
+
+    const attributes = [].concat(req.query.attributes || []);
+
+    const conditions = [].concat(req.query.condition || []);
+
+    let text = "";
+
+
+
+    for (let i = 0; i < attributes.length; i++) {
+
+        text += `    ${conditions[i]} ${attributes[i]};\n`;
+
+    }
+
+
+
+    return text;
+}
+
+
+
+app.get('/class', (req, res) => {
+
+    const generatedCode = generateCode(req);
+
+    res.render('class', { code: generatedCode });
+
 });
 
+
+
 app.listen(3000, () => {
+
     console.log('server is running successfully');
+
     console.log('http://localhost:3000');
+
 });
